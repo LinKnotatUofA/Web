cac668db18693821e99de0ef33a84516f37b2ec5
Sam Bao <sam.b5687@gmail.com>
2015-06-22 03:45:15 -0600

    modulizing navigation bqr

--- parties.php
+++ parties.php
@@ -160,54 +160,7 @@ body {
             </div>
             <div id="row1" class="row" >
                 <div class="span12 offset_special">
-                    <nav class="navigation-bar light">
-                        <div class="navigation-bar-content">
-                            <a href="index.php" class="element"> Home </a>
-                            <span class="element-divider"></span>
-                            <a href="stories.php" class="element"> Story </a>
-                            <span class="element-divider"></span>
-                            <a href="parties.php" class="element"> Party </a>
-                            <span class="element-divider"></span>
-                            <a href="groups.php" class="element"> Groups </a>
-                            <span class="element-divider"></span>
-                            <a href="post.php" class="element"> Post </a>
-                            <span class="element-divider"></span>
-                            <a href="about_us.php" class="element"> About Us </a>
-                            <span class="element-divider"></span>     
-                            <div class="element input-element">
-                                <form>
-                                    <div class="input-control text">
-                                        <input type="text" placeholder="Search...">
-                                        <button class="btn-search"></button>
-                                    </div>
-                                </form>
-                            </div>
-                            <span class="element-divider"></span>
-                             <?php
-                             if(isset($_SESSION['username'])&&$_SESSION['username']!=null)
-                                 {
-                                     echo "<a class='element'> Welcome! ".$_SESSION['username']." </a>" ;
-                                     echo "<div class='element place-right'>
-                                               <a class='dropdown-toggle' href='#'>
-                                                   <span class='icon-cog'></span>
-                                               </a>
-                                               <ul class='dropdown-menu place-right' data-role='dropdown'>
-                                                   <li><a href='#'>Settings</a></li>
-                                                   <li><a href='#'>Edit Profile</a></li>
-                                                   <li><a href='#'>Contacts</a></li>
-                                                   <li><a href='account/logout.php'>Log Out</a></li>
-                                               </ul>
-                                           </div>";
-                                        
-                                 }
-                                 else
-                                     echo"<span class='element-divider place-right'></span>
-                                             <button id='login' class='element image-button image-left place-right'>
-                                                 Log In / Sign Up
-                                             <img src='Assets/default_user.png'/>
-                                             </button>";
-                             ?>
-                    </nav>
+                    <header class="bg-dark" data-load="topbar.php"></header>
                 </div>
             </div>
 
