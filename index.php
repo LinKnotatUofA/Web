96fed3236f48230665c7621668efba3ad4ae3421
Sam Bao <sam.b5687@gmail.com>
2015-06-25 12:16:04 -0600

    fixing conflict on index

--- index.php
+++ index.php
@@ -177,21 +177,3 @@ body {
         
     </body>
 </html>
-
- 
-=======
-<?php
-if(isset($_POST['user']) && isset($_POST['pass'])){
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
-	echo $user;
-	echo '<br>';
-	echo $pass;
-}
-?>
-<form action="Tutorial.php" method="POST">
-User Name <input type="text" name="user"/><br><br>
-Password <input type="password" name="pass" /><br><br>
-<input type="submit" value="submit" />
-</form>
->>>>>>> 358e3d30a451b2598be0773e1163774363d734d8
