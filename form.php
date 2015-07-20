c56ae85a15e1898585099158fbdb3c123ae05544
Rupehra Chouhan <rupehra1@gmail.com>
2015-07-20 14:07:05 -0600

    changed

--- form.php
+++ form.php
@@ -14,17 +14,40 @@ if (mysqli_connect_errno()) {
 
 
 if(isset($_POST['submit'])){
+	
+    $group_name = $_POST['type'];
+	$description  = $_POST['description'];
+	$tags = $_POST['tags'];
+	$ID = 100;
+	$query = mysqli_query($mysqli,"SELECT ID FROM user WHERE ID=$ID");
+	if($query == null){
+		$numrows = 0;
+	}
+	else{
+		$numrows = mysqli_num_rows($query);
+	}
+	while($numrows > 0)
+	{
+	$ID = rand(1,10000);
+	$query = mysqli_query($mysqli,"SELECT GID FROM groups WHERE ID=$ID");
+	#$numrows = mysqli_num_rows($query);
+	}
+	
+	print "<p> Name: $group_name</br>Describe: $description<br>Tags: $tags<br> ID: $ID</p>";
+    
+	
+	$query = mysqli_query($mysqli, "INSERT INTO groups VALUES ('GID','GNAME','GTAGS','GDESCRIPTION') VALUES
+	('$ID','$group_name','$description','$tags')");
 }
+	?>	
 
-
-?>
 <html>
 <body>
     <h3>Group Form</h3>
     <form id="form1" method ="post" enctype="multipart/form-data" action = "form.php">  
         <p>Group Name <input name ="type" type="text" /></p> 
        	<p>Describe what you're gonna do: <input name ="description" type="text" /></p> 
-        <p>Tags: <input name="tags" type="date" /></p>
+        <p>Tags: <input name="tags" type="text" /></p>
         
         <p>
             <label>Upload your image</label>
