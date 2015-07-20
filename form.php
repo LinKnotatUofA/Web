1802e5332f0074f513460a79419a1aa75ac3a6b8
Sam Bao <sam.b5687@gmail.com>
2015-07-20 13:21:45 -0600

    setup for form

--- form.php
+++ form.php
@@ -1,8 +1,27 @@
+<?php 
 
+session_start();
+
+
+$mysqli = new mysqli("localhost", "root", "goodtogo", "bsquared_user");
+
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+/* check connection */
+
+
+if(isset($_POST['submit'])){
+}
+
+
+?>
 <html>
 <body>
     <h3>Group Form</h3>
-    <form id="form1" method ="post" enctype="multipart/form-data">  
+    <form id="form1" method ="post" enctype="multipart/form-data" action = "form.php">  
         <p>Group Name <input name ="type" type="text" /></p> 
        	<p>Describe what you're gonna do: <input name ="description" type="text" /></p> 
         <p>Tags: <input name="tags" type="date" /></p>
--- group_create.php
+++ group_create.php
@@ -71,8 +71,89 @@ body {
                         <div id="featured_row1" class="row" >
                             <div class="spanspecial"> 
                                 </div>
-                            <a href ="form.php"><button class="large">Create</button></a>
-                            <button class="large">Search</button>
+                            <div id ="content_column" class="span4">
+                                
+                                <div class="tile triple double-vertical">
+                                    <div class="carousel" id="carousel1">
+                                            <div class="slide">
+                                                <img src="Assets/1.png" class="cover1" />
+                                            </div>
+                                
+                                            <div class="slide">
+                                                <img src="Assets/2.png" class="cover1" />
+                                            </div>
+                                
+                                            <div class="slide">
+                                                <img src="Assets/3.png" class="cover1"/>
+                                            </div>
+                                
+                                            <a class="controls left"><i class="icon-arrow-left-3"></i></a>
+                                            <a class="controls right"><i class="icon-arrow-right-3"></i></a>
+                                    </div>
+                                    <div class="brand bg-black">
+                                        <div class="label fg-white place-right">Featured</div>
+                                
+                                
+                                    </div>
+                                        <script>
+                                            $(function () {
+                                                $("#carousel1").carousel({
+                                                    height: 250
+                                                });
+                                            })
+                                        </script>
+                                </div>
+
+                                <div class="tile triple double-vertical">
+                                   <div class="carousel" id="carousel2">
+                                           <div class="slide">
+                                               <img src="Assets/1.png" class="cover1" />
+                                           </div>
+                                
+                                           <div class="slide">
+                                               <img src="Assets/2.png" class="cover1" />
+                                           </div>
+                                
+                                           <div class="slide">
+                                               <img src="Assets/3.png" class="cover1"/>
+                                           </div>
+                                
+                                           <a class="controls left"><i class="icon-arrow-left-3"></i></a>
+                                           <a class="controls right"><i class="icon-arrow-right-3"></i></a>
+                                   </div>
+                                   <div class="brand bg-black">
+                                       <div class="label fg-white place-right">Recent</div>
+                                   </div>
+                                   <script>
+                                       $(function (){
+                                               $("#carousel2").carousel({
+                                                   height: 250
+                                               });
+                                           })
+                                       </script>
+                                </div>
+                            </div>
+
+                            
+                            <div id="livefeed_column" class="span4 offset_livefeed_column">
+
+                                <div class="tile triple">
+                                    <h2><a href="?id=1_">"Event #1"</a></h2>
+                                </div>
+
+                                <div class="tile triple">
+                                    <h2><a href="?id=1_">"Event #2"</a></h2>
+                                </div>
+
+                                <div class="tile triple">
+                                    <h2><a href="?id=1_">"Event #3"</a></h2>
+                                </div>
+
+                                <div class="tile triple">
+                                    <h2><a href="?id=1_">"Event #4"</a></h2>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
             </div>
