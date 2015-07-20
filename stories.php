8664c15ba2530f735ad72fcee56250dddf5d8c5f
Dennis <dionysius.truong@ualberta.ca>
2015-07-20 20:50:48 +0000

    hello

--- stories.php
+++ stories.php
@@ -74,21 +74,7 @@ body {
                     <div class="grid fluid show-grid">
                     <!--redo story page pinterest/deviant art style-->  
                     <br><br>            
-                    <div class="tile" >
-            
-                        <span class="text">
-                            <span style="color:black">
-                                <span style="font-weight:bold">
-                                    Testing title
-                                </span>
-                            </span>
-                        </span>
-                        <br>
-                        <span class="text">
-                            <span style="color:black">
-                                Testing content
-                            </span> 
-                    </div>
+                    
 
 					<?php  
 						$tileArray= array('<div class="tile double ribbed-amber">
@@ -130,7 +116,7 @@ body {
       
                     </div>
             </div>
-            <div class="span11 offset_special tertiary-text bg-dark fg-white" style="padding: 20px">
+            <div class="span11 offset_special tertiary-text bg-dark fg-white" style="padding: 20px;bottom:0;right;0">
                 Developed using <a href="http://metroui.org.ua/" class="fg-yellow">Metro UI CSS Template</a> and <a href="http://developer.here.com/api-explorer" class="fg-yellow">Nokia Here Maps</a> by Tech Branch of Bsquared.
                 <br><br> <a href="mailto:UABsquared@gmail.com" class="fg-yellow">Email </a> Us
                 <br><br> Visit Us On <a href="https://github.com/orgs/BsquaredatUofA/" class="fg-yellow">GitHub</a>
