cfb5b23874dbc8e368abbfed69ef8808ca6482c7
Dennis <dionysius.truong@ualberta.ca>
2015-07-20 19:09:00 +0000

    forms

--- form.php
+++ form.php
@@ -3,10 +3,10 @@
 <body>
     <h3>Group Form</h3>
     <form id="form1" method ="post" enctype="multipart/form-data">  
-        <p>Is it for fun or academic? <input name ="type" type="text" /></p> 
+        <p>Group Name <input name ="type" type="text" /></p> 
        	<p>Describe what you're gonna do: <input name ="description" type="text" /></p> 
-        <p>At what time: <input name ="date" type="date" /></p>
-        <p>lat: <input name ="lat" type="number" step="any" id="la"/> long:<input name ="long" type="number" step="any" id="lo"/></p>
+        <p>Tags: <input name="tags" type="date" /></p>
+        
         <p>
             <label>Upload your image</label>
                      <input type="file" name="image"/>       
