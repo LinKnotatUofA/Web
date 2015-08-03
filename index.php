87762071c62323af550135df621a3c3204673688
Dennis <dionysius.truong@ualberta.ca>
2015-08-03 18:49:08 +0000

    hii

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Building Bridges @ UofA - Home</title>
+<title>Sam Bridges @ UofA - Home</title>
 <link rel="shortcut icon" href="Assets/favicon.ico" />
 <meta name="keywords" content="building bridges,b squared,b^2,uofa,u of a,university,of,alberta" />
 <meta name="description" content="B squared is a service provided by the University of Alberta Bridge Builder team to connect new/isolated students with each other." />
