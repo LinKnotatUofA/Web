62f4e79ecc0234c9f0844ba51b4e3ce7d7ed51b0
Dennis <dionysius.truong@ualberta.ca>
2015-08-17 18:30:06 +0000

    hello

--- event.php
+++ event.php
@@ -229,9 +229,10 @@ body {
                                             </label>
                                         </div>   
                                     </form>
-                                     <div class="fb-share-button" data-href="us-cdbr-azure-northcentral-a.cleardb.com/event.php?id=$eventID" data-layout="button_count"></div>
-                                     <div class="g-plusone" href="uofabsquared.azurewebsite.net/event.php?id=$eventID" data-annotation="inline" data-width="300"></div>
-                                     <a href="http://twitter.com/share?url=http://us-cdbr-azure-northcentral-a.cleardb.com/event.php?id=$eventID" class="twitter-follow-button" data-show-count="false">Follow @twitter</a>
+                                     <a href="http://twitter.com/share?url=http://us-cdbr-azure-northcentral-a.cleardb.com/event.php?id=".$eventID class="twitter-follow-button" data-show-count="false">Follow @twitter</a>
+                                     <div class="fb-share-button" data-href="us-cdbr-azure-northcentral-a.cleardb.com/event.php?id=".$eventID data-layout="button_count"></div>
+                                     <div class="g-plusone" href="uofabsquared.azurewebsite.net/event.php?id=".$eventID data-annotation="inline" data-width="300"></div>
+                                     
                                 </ul>
                             </nav>
                         </div>
