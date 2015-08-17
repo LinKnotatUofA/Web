2ebeb276e9471376ed11ebe398793ea10c774f9e
Dennis <dionysius.truong@ualberta.ca>
2015-08-17 18:35:30 +0000

    hello

--- event.php
+++ event.php
@@ -181,14 +181,14 @@ body {
     </script>";
 ?>
     <script src="js/page_scripts/events/event_script.js"></script>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-     <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </head>
 	<body class="metro">
 	    <div id="fb-ba30dbdb2d10ef"></div>
