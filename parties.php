0325488de776f43de2252ae6c2929354c69cf9e7
Rupehra Chouhan <rupehra1@gmail.com>
2015-08-17 12:51:15 -0600

    checking

--- parties.php
+++ parties.php
@@ -233,7 +233,7 @@ body {
                                             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                                                 <rect stroke="white" fill="#ba00ff" x="1" y="1" width="22" height="22" />
                                             </svg></a></li>
-                                        <li><a style="color: white;" onclick="document.location.href = 'http://google.com'" id="own" href="#add">+Your Own</a></li> 
+                                        <li><a style="color: white;" onclick="document.location.href = 'http://uofabsquared.azurewebsites.net/post.php'" id="own" href="#add">+Your Own</a></li> 
                                     </ul> 
                                 </nav>
                             </div>
