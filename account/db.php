519b74ec6a4afec663ecc220219a72a407394870
Sam Bao <sam.b5687@gmail.com>
2015-09-01 02:55:24 -0600

    replaced server url

--- account/db.php
+++ account/db.php
@@ -4,8 +4,6 @@ $mysql_hostname = "us-cdbr-azure-west-c.cloudapp.net";
 $mysql_username = "bea1032a957a19";
 $mysql_password = "c03cc102";
 $mysql_database = "bsquared";
-$mysqli = new mysqli ($mysql_hostname,$mysql_username,$mysql_password,$mysql_database) or die("something is broken");
-
-mysql_select_db($mysql_database,$db) or die("couldn't find database");
+$mysqli = new mysqli ($mysql_hostname,$mysql_username,$mysql_password,$mysql_database);
 
 ?>
