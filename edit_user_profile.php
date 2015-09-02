41564e16b2303d5dd9e4cbdd0759a5686e6eb931
Tan9ster <jianle@ualberta.ca>
2015-09-02 15:47:20 -0600

    try new window

--- edit_user_profile.php
+++ edit_user_profile.php
@@ -159,13 +159,14 @@ body {
                                             overlay: true,
                                             shadow: true,
                                             flat: true,
-                                            icon: '<img src="images/excel2013icon.png">',
+                                            
                                             title: 'Flat window',
                                             content: '',
                                             onShow: function(_dialog){
                                                 var content = _dialog.children('.content');
                                                 content.html('Set content from event onShow');
-                                            }$.Dialog.content(content);
+                                                $.Dialog.content(content);
+                                            }
 
                                         });
                                     }); 
