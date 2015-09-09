f5401f1eb4ce8c3f6217eb498abd9244932ff159
Rupehra Chouhan <rupehra1@gmail.com>
2015-09-09 15:59:46 -0600

    editing this

---
+++ my_profile.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+	
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Building Bridges @ UofA - Home</title>
+<link rel="shortcut icon" href="Assets/favicon.ico" />
+<meta name="keywords" content="building bridges,b squared,b^2,uofa,u of a,university,of,alberta" />
+<meta name="description" content="B squared is a service provided by the University of Alberta Bridge Builder team to connect new/isolated students with each other." />
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<link href="css/metro-bootstrap.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+body {
+	background-color: #3CB6CE;
+    color: #FFFFFF;
+}
+</style>
