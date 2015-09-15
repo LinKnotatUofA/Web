b734f3fd21b1b43bf73d8f19883cbf753aab89f6
Sam Bao <sam.b5687@gmail.com>
2015-09-15 01:30:52 -0600

    email varification test

--- account/registration.php
+++ account/registration.php
@@ -4,6 +4,49 @@
 
 <?php
 require "/phpmailer/class.phpmailer.php";
+require "/phpmailer/class.smtp.php";
+require "/phpmailer/class.pop3.php";
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "uabsquared@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "bsquared";
+
+//Set who the message is to be sent from
+$mail->setFrom('uabsquared@gmail.com', 'Bsquared Registration');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('uabsquared@gmail.com', 'Bsquared Registration');
 $username = @$_POST['user_name'];
 $password = @$_POST['password'];
 $repeatpassword = @$_POST['repeatpassword'];
@@ -78,47 +121,7 @@ if($submit){
                         date_default_timezone_set('Canada/Mountain');
 
                         
-                        //Create a new PHPMailer instance
-                        $mail = new PHPMailer;
-                        
-                        //Tell PHPMailer to use SMTP
-                        $mail->isSMTP();
-                        
-                        //Enable SMTP debugging
-                        // 0 = off (for production use)
-                        // 1 = client messages
-                        // 2 = client and server messages
-                        $mail->SMTPDebug = 2;
-                        
-                        //Ask for HTML-friendly debug output
-                        $mail->Debugoutput = 'html';
-                        
-                        //Set the hostname of the mail server
-                        $mail->Host = 'smtp.gmail.com';
-                        // use
-                        // $mail->Host = gethostbyname('smtp.gmail.com');
-                        // if your network does not support SMTP over IPv6
-                        
-                        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-                        $mail->Port = 587;
-                        
-                        //Set the encryption system to use - ssl (deprecated) or tls
-                        $mail->SMTPSecure = 'tls';
-                        
-                        //Whether to use SMTP authentication
-                        $mail->SMTPAuth = true;
-                        
-                        //Username to use for SMTP authentication - use full email address for gmail
-                        $mail->Username = "uabsquared@gmail.com";
-                        
-                        //Password to use for SMTP authentication
-                        $mail->Password = "bsquared";
-                        
-                        //Set who the message is to be sent from
-                        $mail->setFrom('uabsquared@gmail.com', 'Bsquared Registration');
-                        
-                        //Set an alternative reply-to address
-                        $mail->addReplyTo('uabsquared@gmail.com', 'Bsquared Registration');
+
                         
                         //Set who the message is to be sent to
                         $mail->addAddress($username, '');
