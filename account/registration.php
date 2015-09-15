65e4f97d1ea7a91abd30a2a3eab42595ace300f9
Sam Bao <sam.b5687@gmail.com>
2015-09-15 02:01:13 -0600

    fixing phpmailer

--- account/registration.php
+++ account/registration.php
@@ -3,11 +3,50 @@
 <head>
 
 <?php
+require "class.phpmailer.php";
+
+
 //Create a new PHPMailer instance
-require"/phpmailer/PHPMailerAutoload.php";
-$mail = new PHPMailer();
+$mail = new PHPMailer;
 
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
 
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "uabsquared@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "bsquared";
+
+//Set who the message is to be sent from
+$mail->setFrom('uabsquared@gmail.com', 'Bsquared Registration');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('uabsquared@gmail.com', 'Bsquared Registration');
 $username = @$_POST['user_name'];
 $password = @$_POST['password'];
 $repeatpassword = @$_POST['repeatpassword'];
@@ -79,6 +118,33 @@ if($submit){
                             echo "registration successful";
                         }
                         
+                        date_default_timezone_set('Canada/Mountain');
+
+                        
+
+                        
+                        //Set who the message is to be sent to
+                        $mail->addAddress($username, '');
+                        
+                        //Set the subject line
+                        $mail->Subject = 'PHPMailer GMail SMTP test';
+                        
+                        //Read an HTML message body from an external file, convert referenced images to embedded,
+                        //convert HTML into a basic plain-text alternative body
+                        //$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+                        
+                        //Replace the plain text body with one created manually
+                        $mail->AltBody = 'This is a plain-text message body';
+                        
+                        //Attach an image file
+                        //$mail->addAttachment('images/phpmailer_mini.png');
+                        
+                        //send the message, check for errors
+                        if (!$mail->send()) {
+                            echo "Mailer Error: " . $mail->ErrorInfo;
+                        } else {
+                            echo "Message sent!";
+                        }
 
                         
                         /*if(@mail($username, 'Bsquared', $message))
