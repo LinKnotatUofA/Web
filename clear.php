f6a4a7ef727d53c6db0c5830e08bbda547fa8b58
Rupehra Chouhan <rupehra1@gmail.com>
2015-09-16 16:28:26 -0600

    verification.php

---
+++ clear.php
@@ -0,0 +1,29 @@
+<?php 
+	$mysqli = new mysqli("us-cdbr-azure-west-c.cloudapp.net", "bea1032a957a19", "c03cc102", "bsquared");
+	
+	$dates =  mysqli_query($mysqli, "SELECT TIME FROM event");
+	$date_array = mysqli_fetch_array($dates);
+	$length =  mysqli_fetch_lengths($dates);
+	for ($i=0; $i < $length; $i++){
+		printf("$date_array[$i]");
+		printf("<br>");
+	}
+	
+	mysqli_close($mysqli);
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
