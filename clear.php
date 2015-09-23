8859fa5a7cbdfcf17a1a183c8c20e1773b1e7a5c
Rupehra Chouhan <rupehra1@gmail.com>
2015-09-23 16:02:49 -0600

    testing clear.php

--- clear.php
+++ clear.php
@@ -2,7 +2,7 @@
 	$mysqli = new mysqli("us-cdbr-azure-west-c.cloudapp.net", "bea1032a957a19", "c03cc102", "bsquared");
 	
 	$dates =  mysqli_query($mysqli, "SELECT TIME FROM event");
-	$date_array = mysqli_fetch_assoc($dates);
+	$date_array = mysqli_fetch_row($dates);
 	$length =  mysqli_fetch_lengths($dates);
 	printf("Len = $length ");
 	echo "yes";
