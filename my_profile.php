6793565c9f17aabb0fb2f9139863cf1f1688fe48
Tan9ster <jianle@ualberta.ca>
2015-09-30 15:47:28 -0600

    tiles

--- my_profile.php
+++ my_profile.php
@@ -92,7 +92,7 @@ body {
                          echo " <a class=\"tile double bg-orange\" data-click=\"transform\">
                                             <div class=\"tile-content\">
                                                 <div class=\"text-left padding10 ntp\">
-                                                    <h2 class=\"fg-white no-margin\">";echo "Birthday: ";echo"</h2>
+                                                    <h2 class=\"fg-white no-margin\">";print_r($info['firstn']);print_r($info['lastn']);echo "Birthday: ";echo"</h2>
                                                     <p class=\"fg-white\">";print_r ($info['birthdays']); echo "<br>";echo "Study:";echo"</p>
                                                 </div>
                                             </div>
