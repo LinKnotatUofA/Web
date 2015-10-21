89157fe686f52b92e7cdd1839202d00e119c744c
Sam Bao <sam.b5687@gmail.com>
2015-10-21 13:05:13 -0600

    facelifting the site

--- about_us.php
+++ about_us.php
@@ -68,7 +68,11 @@ body {
                 <div class="row span12" id="content" style="width: 100%; height: auto; background: #C7D28A" style="padding:40px"/>
                     <div class="grid fluid show-grid">
                     
-                    <p>for recruiting and the marketing-savy</p>              
+                    <p>Linknot orginated from an idea that sprout out of the undergraduate research initive in October of 2014, to connect isolated students on campus, the focus was to provide a place to lend them a voice to be heard and a platform to connect on.</p>  
+                    <p>The organization was orignall called Stories With Out Borders(SWB), later SWB collaborated with Another organization and was renamed to Bulding Bridges @ University of Alberta, we existed as a student group within student union up till fall of 2015. And fast forward this october plans were made to commercialize the platform and now it is LinKnot</p>
+                    <p>Although we shifted our brand quite a bit, the mission stays the same, we strive to provide students from post-secondary instituion an exclusive platform to connect with each other, either by sharing their stories, or even better, sharing their time together.</p>
+                    <p>Be it studying for an exam during the final week, or getting wasted on a Friday night out .We believe each student would enjoy a better university experience if they were not isolated</p>
+               
                     
                     </div>
             </div>
